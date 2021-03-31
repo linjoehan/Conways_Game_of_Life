5896d8f2863dc3a34b9c06d3dd3e57111f354149
linjoehan <linjoehan@yahoo.com>
2021-03-31 17:04:58 +0200

    Added boilerplate for navigation menus

--- src/ConwaysViewer.java
+++ src/ConwaysViewer.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class ConwaysViewer
 {
+    Scanner scanner;
     ConwaysByLinjoehan gamestate;
+    boolean running; // Keeps the programme running
+    int updateframes; // Tracks the number of frames to run
     
     public static void main(String args[])
     {
@@ -10,15 +16,92 @@ public class ConwaysViewer
     
     private void run()
     {
+        //init class variables
+        scanner = new Scanner(System.in);
         gamestate = new ConwaysByLinjoehan();
+        running = true;
+        updateframes = 0;
         
         intro();
-        gamestate.print();
+        
+        //gameloop
+        while(running)
+        {
+            gamestate.print();
+            if(updateframes > 0)
+            {
+                //gamestate.updateFrame();
+                updateframes--;
+            }
+            else
+            {
+                menu();
+            }
+            wait(1000);
+        }
     }
     
     private void intro()
     {
         System.out.println("Conways Game of Life by linjoehan");
         System.out.println("For best experience maximise this window");
+        System.out.println("Press Enter to continue...");
+        scanner.nextLine();
+    }
+    
+    private void menu()
+    {
+        System.out.println(" 1 - Load predefined state");
+        System.out.println(" 2 - Edit cell in state manually");
+        System.out.println(" 3 - Run simulation for a selected number of frames");
+        System.out.println(" 0 - Exit");
+        System.out.print("Enter Option number:");
+        int option;
+        try
+        {
+            option = scanner.nextInt();
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.println("Input is not a recognised option");
+            return;
+        }
+        
+        switch(option)
+        {
+            case 0 :
+            {
+                System.out.println("Thanks for playing... Exiting");
+                running = false;
+            }break;
+            case 1 :
+            {
+                
+            }break;
+            case 2 :
+            {
+                
+            }break;
+            case 3 :
+            {
+                
+            }break;
+            default :
+            {
+                System.out.println("Input is not a recognised option");
+            }
+        }
+    }
+    
+    private void wait(int ms)
+    {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 }
